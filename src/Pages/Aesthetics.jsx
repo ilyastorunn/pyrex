4fb883236dcde6b6610aacfea4ac9e5842ce763e
İlyas Torun <your-email@example.com>
2025-05-10 01:32:31 +0300

    minor updates

--- src/Pages/Aesthetics.jsx
+++ src/Pages/Aesthetics.jsx
@@ -2,72 +2,98 @@ import pic1 from "../assets/pic1.jpeg";
 import pic2 from "../assets/pic2.jpeg";
 import pic3 from "../assets/pic3.jpeg";
 import pic4 from "../assets/pic4.jpeg";
+import pic5 from "../assets/pic5.JPG";
 
 export default function Aesthetics() {
   return (
     <>
-      <div className="grid grid-cols-4 grid-rows-4 gap-4 bg-gray-700 p-8">
+      <div className="grid grid-cols-5 grid-rows-5 gap-4 bg-gray-700 p-8 min-h-screen">
+        <div className="col-span-1 row-span-2 [perspective:1000px]">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
+              <img
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black flex items-center justify-center">
+              <p className="text-2xl font-bold text-white">Minimal Art</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-2 row-span-1 [perspective:1000px]">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
+              <img
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black rounded-lg flex items-center justify-center">
+              <p className="text-2xl font-bold text-white">Urban Style</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-2 row-span-1 [perspective:1000px]">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
+              <img
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black rounded-lg flex items-center justify-center">
+              <p className="text-2xl font-bold text-white">Nature View</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 row-span-1 [perpective:1000px]">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
+              <img
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black rounded-lg flex items-center justify-center">
+              <p className="text-xl font-bold text-white">Modern</p>
+            </div>
+          </div>
+        </div>
+
         <div className="col-span-2 row-span-2 [perspective:1000px]">
-          <div className="relative w-[400px] h-[600px] transition-transform duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
-            <div className="absolute w-full h-full">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
               <img
-                src={pic1}
-                alt="1"
-                className="w-full h-full object-cover rounded-lg"
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
               />
             </div>
-            <div className="absolute w-full h-full bg-black/90 rounded-lg [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              <div className="flex items-center justify-center h-full text-white p-8 text-center">
-                <p className="text-2xl font-semibold">Estetik Görüntü 1</p>
-              </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black rounded-lg flex items-center justify-center">
+              <p className="text-3xl font-bold text-white">Abstract</p>
             </div>
           </div>
         </div>
-        <div className="col-span-2 row-span-2 col-start-3 [perspective:1000px]">
-          <div className="relative w-[400px] h-[600px] transition-transform duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
-            <div className="absolute w-full h-full">
+
+        <div className="col-span-1 row-span-2 [perspective:1000px]">
+          <div className="relative w-full h-full transition-all duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
+            <div className="absolute w-full h-full [backface-visibility:hidden]">
               <img
-                src={pic2}
-                alt="1"
-                className="w-full h-full object-cover rounded-lg"
+                src={pic5}
+                alt="Aes1"
+                className="w-full h-full object-cover"
               />
             </div>
-            <div className="absolute w-full h-full bg-black/90 rounded-lg [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              <div className="flex items-center justify-center h-full text-white p-8 text-center">
-                <p className="text-2xl font-semibold">Estetik Görüntü 2</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-2 row-span-2 [perspective:1000px]">
-          <div className="relative w-[400px] h-[600px] transition-transform duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
-            <div className="absolute w-full h-full">
-              <img
-                src={pic3}
-                alt="1"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
-            <div className="absolute w-full h-full bg-black/90 rounded-lg [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              <div className="flex items-center justify-center h-full text-white p-8 text-center">
-                <p className="text-2xl font-semibold">Estetik Görüntü 3</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-2 row-span-2 col-start-3 [perspective:1000px]">
-          <div className="relative w-[400px] h-[600px] transition-transform duration-700 transform-style-preserve-3d hover:[transform:rotateY(180deg)]">
-            <div className="absolute w-full h-full">
-              <img
-                src={pic4}
-                alt="1"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
-            <div className="absolute w-full h-full bg-black/90 rounded-lg [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              <div className="flex items-center justify-center h-full text-white p-8 text-center">
-                <p className="text-2xl font-semibold">Estetik Görüntü 4</p>
-              </div>
+            <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] bg-black rounded-lg flex items-center justify-center">
+              <p className="text-2xl font-bold text-white">Vintage</p>
             </div>
           </div>
         </div>
